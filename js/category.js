6f8592f4e0a5077a36290b65ce3605695fe3e976
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2018-11-22 12:22:48 +0100

    category page, done with the styling and ajax

--- js/category.js
+++ js/category.js
@@ -16,20 +16,25 @@ class Foodog extends HTMLElement {
     
     set doc(doc) {
         this.innerHTML = `
-        <div class="container" style="margin-left:140px;">
-        <div class="row" style="border-bottom:1px solid black;margin-bottom: 15%;">
-            <div class="col" style="margin-bottom:6%">
-                <a href="#"><img src=${doc.imgUrl} class="img-fluid"></a>
+        <div class="category-div">
+        <div class="category-img">
+            <a href="#"><img src=${doc.imgUrl}></a>
+        </div>
+        <div class="category-subject">
+            <a href="#" class="category-title"> ${doc.tagForArticle} |</a>
+            <h3 class="category-header"><a href="#">${doc.title}</a></h3>
+            <p class="category-paragraph">${doc.text}</p>
+            <div class="category-share">
+                <div class="share-icon"><a href="#"><i class="fas fa-share"></i> Share</a></div>
+                <div class="round-buttons">
+                    <a href="www.facebook.com"><i class="fab fa-facebook"></i></i></a>
+                    <a href="https://twitter.com/intent/tweet?text=Superfoods+in+Your+Kitchen%3A+Carrots+for+Dogs&url=http%3A%2F%2Fdigest.thefarmersdog.com%2Fsuperfoods-kitchen-carrots-dogs%2F&via=farmersdog"><i class="fab fa-twitter"></i></a>
+                    <a href="https://www.pinterest.com/pin/24980972918988287/?showsignup=1"><i class="fab fa-pinterest"></i></a>
+                </div>
             </div>
-            <div class="col" style="margin-bottom:6%;padding-left:4%">
-                <h6 style="margin-bottom: 7%"><a href="#" class="nutrition-link">${doc.tagForArticle[1]} |</a></h6>
-                <h4 style="margin-bottom: 7%"><a href="#" class="header-link">${doc.title}</a></h4>
-                <p style="line-height: 3"><a href="#" class="paragraph-link">${doc.title}</a></p>
-                <div class="share-icon" style="margin-top: 11%"><i class="fas fa-share" style="border: 1px solid #cbcbcb;border-radius:50%;padding:14px"></i> Share</div>
-            </div>
-            
         </div>
     </div>
+    <div class="bottom-line"></div>
         `
     }
 }
