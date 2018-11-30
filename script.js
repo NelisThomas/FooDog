e7350091bd0ced6fbd1619227ffec286b5c85a49
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2018-11-30 09:12:36 +0100

    clean up

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
+let popupButton = document.querySelector(".top-bar-button");
+let popupButtonClose = document.querySelector(".popup-menu-close");
+let popupMenu = document.querySelector(".popup-menu");
+
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("searchTrigger");
 
-// When the user clicks the button, open the modal 
-function showModal () {
-    modal.style.display = "block";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+popupMenu.style.display = 'none';
+let menuButton = false;
+let menuButtonClose = false;
+
+let displayMenu = () => {
+    if (menuButton === false) {
+        popupMenu.style.display = 'flex';
+        popupMenu.style.zIndex = '10';
     }
 }
+let closeMenu = () => {
+    if (menuButtonClose === false) {
+        popupMenu.style.display = 'none';
+        popupMenu.style.zIndex = '-1';
+    } 
+}
+
+popupButton.addEventListener("click", displayMenu);
+popupButtonClose.addEventListener("click", closeMenu);
